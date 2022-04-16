4f01478a8cbb43ca2df70988ef6c9aa7fcdc8783
JRArocha <johnroche.arocha@cvsu.edu.ph>
2022-04-16 08:20:32 +0800

    Updated with flag

--- user.py
+++ user.py
@@ -2,14 +2,25 @@ import cv2
 import os
 
 while True:
-    fileName = input("\nadd '.jpg' \nFile name: ")
+    file = input(" \nFile name: ")
+    fileName = str(file + '.jpg')
 
-    if os.path.exists(fileName):
-        image = cv2.imread(fileName,1)
-        cv2.imshow(fileName, image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        break
+    flag = int(input("[1] Colored [2] Grayscale \nEnter Flag Values: "))
+
+    if flag == 1:
+        if os.path.exists(fileName):
+            image = cv2.imread(fileName,1)
+            cv2.imshow(fileName, image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            break
+    elif flag == 2:
+        if os.path.exists(fileName):
+            image = cv2.imread(fileName,0)
+            cv2.imshow(fileName, image)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+            break
 
     else:
         print("File not found...")
